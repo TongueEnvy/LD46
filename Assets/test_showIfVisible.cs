7d12757605cf864fef41e2cebacb7088334f8e0c
TongueEnvy <mr.dfelkins@gmail.com>
2020-04-17 21:17:40 -0700

    Created Project Files

---
+++ Assets/test_showIfVisible.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_showIfVisible : MonoBehaviour
+{
+
+    public Material greenMat;
+    public Material redMat;
+
+    public MeshRenderer mRender;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (GetComponent<test_checkIfVisible>().inView)
+        {
+
+            if(mRender.sharedMaterials[0] != greenMat)
+            {
+
+                mRender.sharedMaterials[0] = greenMat;
+
+            }
+
+        }
+        else
+        {
+
+            if (mRender.sharedMaterials[0] != redMat)
+            {
+
+                mRender.sharedMaterials[0] = redMat;
+
+            }
+
+        }
+
+    }
+}
