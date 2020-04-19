ff53acd75c1e96246c8ba1ecc9c10436cbd63ae9
TongueEnvy <mr.dfelkins@gmail.com>
2020-04-18 18:18:31 -0700

    Added monster and insanity effects.

---
+++ Assets/ui_jitter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ui_jitter : MonoBehaviour
+{
+    public float xJitter;
+    public float yJitter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float xPos = Random.Range(-xJitter, xJitter);
+        float yPos = Random.Range(-yJitter, yJitter);
+
+        GetComponent<RectTransform>().anchoredPosition = new Vector2(xPos, yPos);
+
+    }
+}
